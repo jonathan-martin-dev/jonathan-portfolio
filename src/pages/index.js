57cbf4c2fae98cc0eca037ed529163fde2869add
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-07-06 17:18:18 -0700

    package updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,12 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    
   </Layout>
 )
 
