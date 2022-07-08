679fd1dac589f4a435f1102d5cdb7621d27ff719
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-07-08 13:01:15 -0700

    Update AboutMe

--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -1,7 +1,25 @@
 import React from "react"
 
 const WorkHistory = () => {
-  return <>WorkHistory</>
+  return (
+    <>
+      <div className="grid grid-flow-col">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Your Email</span>
+          </label>
+          <label class="input-group">
+            <span>Email</span>
+            <input
+              type="text"
+              placeholder="info@site.com"
+              class="input input-bordered"
+            />
+          </label>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default WorkHistory
